1cbea479511dbed0cb90bde872371885088d779a
Merwin Mathew <merwin.mathew@erasmith.com>
2025-06-05 22:56:40 +0530

    edited restore-dump to fetch empty dump file

--- restore-dump.sh
+++ restore-dump.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "Restoring database from custom-format dump..."
-psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/pgbackup.dump
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/pgdumplatest.dump
 echo "Database restored successfully."
